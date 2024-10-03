e87bcf2123bdb0061dc641eace52a46bdf021cdb
Milan_sacahni <milan.sachani.ms@gmail.com>
2024-10-03 14:59:26 +0200

    📝 (Home.tsx): add deployment rocket emojis to indicate upcoming blog updates The deployment rocket emojis were added to the text in the Home page component to visually indicate that there will be upcoming blog updates. This change adds a playful and engaging element to the message, encouraging users to stay tuned for more content.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const HomePage = () => {
     <main className="flex flex-col gap-4 items-center py-4">
       <h1 className="text-4xl font-bold">Welcome to My Blog Page!!</h1>
       <p className="text-lg">
-        The Blogs are coming soon. Stay tuned for more updates.
+        The Blogs are coming soon. Stay tuned for more updates. 🚀🚀🚀
       </p>
     </main>
   );
