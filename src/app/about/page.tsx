6fddb30217326417275c86b3ee8b4f5a2139ade3
Milan_sacahni <milan.sachani.ms@gmail.com>
2024-10-05 15:44:24 +0200

    📝 (page.tsx): update SEO metadata for the About Me page to provide more detailed information about the author and the blog.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,10 +3,36 @@
 import AboutMePage from "@/pages/AboutMePage/AboutMePage";
 import { getMetadata } from "@/utils/getMetadata";
 
-// Define the metadata at the server level
+// SEO metadata for the home page
 export const metadata = getMetadata({
   title: "About me - My Blog",
-  description: "Learn more about the author of this blog.",
+  description:
+    "Learn more about Milan Sachani, a Software Engineer, and the creator of this blog. Explore the latest articles on web development, JavaScript, and more.",
+  keywords: ["blog", "Next.js", "SEO"],
+  author: "Milan Sachani",
+  robots: "index,follow",
+  viewport: "width=device-width, initial-scale=1",
+  canonical: "https://blog.milansachani.dev/",
+  openGraph: {
+    title: "About me - My Blog",
+    description:
+      "Learn more about Milan Sachani, a Software Engineer, and the creator of this blog. Explore the latest articles on web development, JavaScript, and more.",
+    url: "https://blog.milansachani.dev/",
+    type: "website",
+    images: [
+      {
+        url: "/images/seo.png",
+        alt: "A minimalistic blog banner for SEO purposes representing web development and JavaScript.",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Welcome to My Blog",
+    description:
+      "Explore the latest articles on web development, JavaScript, and more.",
+    images: ["/images/seo.png"],
+  },
 });
 
 // Import the client component here, ensuring metadata is separate
