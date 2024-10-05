93d39d82905851a7302c4141666e99c579b67525
Milan_sacahni <milan.sachani.ms@gmail.com>
2024-10-05 15:56:20 +0200

    ✨ (page.tsx): add SEO metadata for the home page to improve search engine optimization and provide better information about the blog and its content.

--- src/app/tags/page.tsx
+++ src/app/tags/page.tsx
@@ -1,6 +1,39 @@
 // apps/tags/page.tsx
 import Link from "next/link";
 import { getAllTags } from "@/utils/getAllPosts";
+import { getMetadata } from "@/utils/getMetadata";
+
+// SEO metadata for the home page
+export const metadata = getMetadata({
+  title: "Tags - Milan's Blog",
+  description:
+    "Explore the latest articles on web development, JavaScript, and more.",
+  keywords: ["blog", "Next.js", "SEO"],
+  author: "Milan Sachani",
+  robots: "index,follow",
+  viewport: "width=device-width, initial-scale=1",
+  canonical: "https://blog.milansachani.dev/",
+  openGraph: {
+    title: "Tags - Milan's Blog",
+    description:
+      "Learn more about Milan Sachani, a Software Engineer, and the creator of this blog. Explore the latest articles on web development, JavaScript, and more.",
+    url: "https://blog.milansachani.dev/",
+    type: "website",
+    images: [
+      {
+        url: "/images/seo.png",
+        alt: "A minimalistic blog banner for SEO purposes representing web development and JavaScript.",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Tags - Milan's Blog",
+    description:
+      "Explore the latest articles on web development, JavaScript, and more.",
+    images: ["/images/seo.png"],
+  },
+});
 
 export default function TagsPage() {
   const tags = getAllTags();
