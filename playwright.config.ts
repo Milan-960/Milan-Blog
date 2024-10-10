6027af42e372183f28ae3bc7657f33e413c19e2b
Milan_sacahni <milan.sachani.ms@gmail.com>
2024-10-10 22:20:44 +0200

    🚀 (playwright.yml): add deployment step to deploy Playwright report to GitHub Pages upon successful workflow run ♻️ (playwright.config.ts): refactor baseURL variable to use NEXT_PUBLIC_BASE_URL environment variable for consistency with Next.js conventions

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.PLAYWRIGHT_BASE_URL || "http://localhost:3000", // use preview URL or local server
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000", // use preview URL or local server
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
